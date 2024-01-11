4a5606aca0b7612cd7be3a8e1ab788a34758f0df
Nusratillo <nxursanjonov888@gmail.com>
2024-01-11 17:51:31 +0500

    node-modules delete two

--- client/main.js
+++ client/main.js
@@ -1,14 +1,23 @@
 const box = document.querySelector("#box");
 
-const baseURL = "http://localhost:3000"
+const baseURL = "http://localhost:3000";
 
-const getFoods = async ()=> {
-    try {
-        const res = await fetch(`${baseURL}/foods`)
-        const data = await res.json()
-        console.log(data);
-    } catch (e) {
-        console.log(e);
-        
-    }
-}
+const getUsers = async () => {
+  try {
+    const res = await fetch(`${baseURL}/foods`);
+    console.log(res);
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    // console.log(error);
+  }
+};
+
+const renderData = async () => {
+  const date = await getUsers();
+  console.log(date);
+  box.innerHTML = date?.map((product) => `<div class="cards">${product.title}</div>`).join("");
+};
+
+renderData();
